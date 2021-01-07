79a03a4d60f416dfa3d9bd69dc245476614f403c
hchX009 <hecenhao@outlook.com>
2021-01-07 23:45:40 +0800

    commit RestaurantDataAnalysis

---
+++ RestaurantDataAnalysis/get_analysis_restaurant_data.py
@@ -0,0 +1,14 @@
+import xlrd
+import xlwt
+
+def read_purchase_data():
+    return
+
+def read_restaurant_data():
+    return
+
+def write_analysis_purchase_data():
+    return
+
+def write_analysis_restaurant_data():
+    return
