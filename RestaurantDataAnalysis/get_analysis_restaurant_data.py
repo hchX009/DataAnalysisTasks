16b50c02fa5759555d7559f001afc4e62c29e09b
hchX009 <hecenhao@outlook.com>
2021-01-10 23:26:16 +0800

    commit write_analysis_restaurant_data(Excel, new_sheet) and write_analysis_purchase_data(Excel, new_sheet) in 20210110

--- RestaurantDataAnalysis/get_analysis_restaurant_data.py
+++ RestaurantDataAnalysis/get_analysis_restaurant_data.py
@@ -14,7 +14,7 @@ def read_purchase_data():
         sheet_row_dict['购买人数'] = sheet.cell(row, 4).value
         evaluation = sheet.cell(row, 5).value
         if evaluation == '暂无评价':
-            sheet_row_dict['团购评价'] = ''
+            sheet_row_dict['团购评价'] = 0
         else:
             sheet_row_dict['团购评价'] = evaluation
         discount_value = sheet.cell(row, 8).value
@@ -26,7 +26,7 @@ def read_purchase_data():
         sheet_row_dict['团购价'] = discount_value
         sheet_row_dict['市场价'] = market_value
         new_sheet.append(sheet_row_dict)
-        print(sheet_row_dict)
+        # print(sheet_row_dict)
     return new_sheet
 
 
@@ -51,17 +51,54 @@ def read_restaurant_data():
         sheet_row_dict['行政区'] = area
         sheet_row_dict['评分'] = sheet.cell(row, 3).value
         new_sheet.append(sheet_row_dict)
-        print(sheet_row_dict)
+        # print(sheet_row_dict)
     return new_sheet
 
 
-def write_analysis_purchase_data(new_sheet):
+def write_analysis_purchase_data(Excel, new_sheet):
+    sheet = Excel.add_sheet('Sheet1')
+    sheet.write(0, 0, '店名')
+    sheet.write(0, 1, '团购名')
+    sheet.write(0, 2, '购买人数')
+    sheet.write(0, 3, '团购评价')
+    sheet.write(0, 4, '团购价')
+    sheet.write(0, 5, '市场价')
+    row = 1
+    for sheet_row in new_sheet:
+        sheet.write(row, 0, sheet_row['店名'])
+        sheet.write(row, 1, sheet_row['团购名'])
+        sheet.write(row, 2, sheet_row['购买人数'])
+        sheet.write(row, 3, sheet_row['团购评价'])
+        sheet.write(row, 4, sheet_row['团购价'])
+        sheet.write(row, 5, sheet_row['市场价'])
+        row += 1
+    Excel.save('餐饮团购数据整理.xls')
     return
 
 
-def write_analysis_restaurant_data(new_sheet):
+def write_analysis_restaurant_data(Excel, new_sheet):
+    sheet = Excel.add_sheet('Sheet2')
+    sheet.write(0, 0, '店名')
+    sheet.write(0, 1, '关键词')
+    sheet.write(0, 2, '城市')
+    sheet.write(0, 3, '行政区')
+    sheet.write(0, 4, '评分')
+    row = 1
+    for sheet_row in new_sheet:
+        sheet.write(row, 0, sheet_row['店名'])
+        sheet.write(row, 1, sheet_row['关键词'])
+        sheet.write(row, 2, sheet_row['城市'])
+        sheet.write(row, 3, sheet_row['行政区'])
+        sheet.write(row, 4, sheet_row['评分'])
+        row += 1
+    Excel.save('餐饮团购数据整理.xls')
     return
 
 
 if __name__ == '__main__':
-    read_restaurant_data()
+    Excel = xlwt.Workbook()
+    sheet = read_purchase_data()
+    write_analysis_purchase_data(Excel, sheet)
+    sheet = read_restaurant_data()
+    write_analysis_restaurant_data(Excel, sheet)
+    print('Finished!')
