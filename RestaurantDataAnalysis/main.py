6ff984e5570f344463cc39f8c14db35e3f33b45d
hchX009 <hecenhao@outlook.com>
2021-03-07 22:58:37 +0800

    20210307 hchX009 commit : 2.1

--- RestaurantDataAnalysis/main.py
+++ RestaurantDataAnalysis/main.py
@@ -2,6 +2,8 @@ from get_participation_restaurant import *
 from get_favour_catering_type import *
 from get_analysis_restaurant_data import *
 from get_restaurant_sale_num import *
+from show_discount_relationship import *
+from show_good_restaurant_num import *
 
 
 if __name__ == '__main__':
@@ -16,5 +18,9 @@ if __name__ == '__main__':
     print_top5_catering_type(purchase_data_sheet)
 
     print_top10_restaurant_sale(purchase_data_sheet)
+
+    show_discount_relationship_with_sale_num(purchase_data_sheet)
+
+    show_good_restaurant_num_with_area(restaurant_data_sheet)
 
     print('Finished!')
